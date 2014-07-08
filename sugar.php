e10926b91e07f6f69298c78f7866daaecafae272
Tomáš Bončo <tomasbonco@gmail.com>
2014-07-08 14:30:46 +0200

    Created function sugar, added new methods

--- sugar.php
+++ sugar.php
@@ -5,84 +5,158 @@
 # Licence: MIT
 # GitHub: https://github.com/tomasbonco/sugar_php
 
-# Ver: 0.1.1
+# Ver: 0.2
 # Date: 28.6.2014
 
 
 function describe( $desc, $callback )
 {
-	global $before_each, $after_each;
+	global $__sugar;
 
-	$before_each = function(){};
-	$after_each = function(){};
+	if ( empty( $desc ) || ! is_string( $desc ) || strlen( trim( $desc )) == 0 ) throw new Exception( 'First parameter of it() is required and must be string.' );
+	if ( empty( $callback ) || ! is_callable( $callback) ) throw new Exception( 'Second parameter of it() is required and must be callable.' );
 
-	printf( '<div class="describe"> <h2> %s </h2> <div class="its">', $desc );
+	if ( empty( $__sugar ) )
+	{
+		$__sugar = new stdClass();
+	}
 
-	$callback();
+	if ( empty( $__sugar->tests) )
+	{
+		$__sugar->tests = array();
+	}
 
-	printf( '</div> </div>');
+	$__sugar->tests[ $desc ] = $callback;
 
-	$before_each = function(){};
-	$after_each = function(){};
 }
 
 function it( $desc, $callback )
 {
-	global $before_each, $after_each;
+	global $__sugar;
+
+	if ( empty( $desc ) || ! is_string( $desc ) || strlen( trim( $desc )) == 0 ) throw new Exception( 'First parameter of it() is required and must be string.' );
+	if ( empty( $callback ) || ! is_callable( $callback) ) throw new Exception( 'Second parameter of it() is required and must be callable.' );
+
+	if ( empty( $__sugar->tests )) throw new Exception( 'It() must be called inside describe().');
+
+	$before_each = $__sugar->before_each; 	# Funny fact: PHP can't do $__sugar->before_each(), because before_each wasn't defined as a method
+	$after_each = $__sugar->after_each;
 
 	$before_each();
 
-	$status = 'OK';
-	$error = NULL;
+	$report = new stdClass;
 
 	try
 	{
 		$callback();
+		$report->status = 'OK';
 	}
 
 	catch ( Exception $e)
 	{
-		$status = 'FAIL';
-		$stack = $e->getTrace();
-
-		$stack_string = '';
-		array_walk( $stack, function( $item ) use( &$stack_string )
+		$report->status = 'FAIL';
+		$report->stack = array();
+		
+		array_walk( $e->getTrace(), function( $item )
 		{
 			if ( ! empty( $item['file'] ) && ! empty( $item['function'] ) && ! empty( $item['line'] ) )
 			{
 				if ( $item['file'] != __FILE__ )
 				{
-					$stack_string .= sprintf( "%s at %s:%s\n<br>", $item['function'], $item['file'], $item['line'] );
+					$report->stack[] = sprintf( "%s at %s:%s", $item['function'], $item['file'], $item['line'] );
 				}
 			}
 		});
-
-		$error = sprintf( "<div class='error'> <div class='error-message'><strong>%s</strong></div> <div class='trace'>%s</div> </div>\n", $e->getMessage(), $stack_string);
 	}
 
-	printf( "<div class='it'><span class='test-name'>%s</span> ................... <span class='test-result %s'>%s</span> \n<br> %s </div>", $desc, strtolower( $status ), $status, $error );
+	$__sugar->current_block[ $desc ] = $report;
 
 	$after_each();
 }
 
 function a( $value )
 {
-	return new Unit_test( $value );
+	return new Sugar_unit_test( $value );
 }
 
 function before_each( $callback )
 {
-	global $before_each;
-	$before_each = $callback;
+	global $__sugar;
+	$__sugar->before_each = $callback;
 }
 
 function after_each( $callback )
 {
-	global $after_each;
-	$after_each = $callback;
+	global $__sugar;
+	$__sugar->after_each = $callback;
 }
 
-class Unit_test
+function sugar( $filter = NULL, $output = TRUE, $reporter = 'sugar_report' )
+{
+	global  $__sugar;
+
+	if ( ! empty( $filter ) && ! is_array( $filter )) throw new Exception( 'First parameter ($filter) of Sugar must be an array (or null)!' );
+	if ( ! empty( $output ) && ! is_bool( $output )) throw new Exception( 'Second parameter ($output) of Sugar must be bool!' );
+
+
+
+
+	if ( empty( $__sugar ) )
+	{
+		return FALSE;
+	}
+
+
+	$tests = $__sugar->tests;
+
+	if ( ! empty( $filter ) )
+	{
+		array_walk( $tests, function( &$value, $key ) use( $filter ) { if ( ! in_array( $key, $filter )) $value = NULL; });
+		$tests = array_filter( $tests );
+	}
+
+	foreach ( $tests as $test => $callback )
+	{
+		$__sugar->before_each = function(){ echo 'aa'; };
+		$__sugar->after_each = function(){};
+
+		$__sugar->results[ $test ] = array();
+		$__sugar->current_block = & $__sugar->results[ $test ];
+
+		$callback();
+
+		$__sugar->before_each = function(){};
+		$__sugar->after_each = function(){};
+	}
+
+	if ( ! empty( $reporter ))
+	{
+		if ( ! is_callable( $reporter ) ) throw new Exception( sprintf( 'Reporter must callable, "%s" is not.', $reporter ));
+		$reporter();
+	}
+
+	return $__sugar->results;
+}
+
+
+function sugar_report()
+{
+	global $__sugar;
+
+	foreach ( $__sugar->results as $desc => $results )
+	{
+		printf( '<div class="describe"> <h2> %s </h2> <div class="its">', $desc );
+
+		foreach ( $results as $it => $report )
+		{
+			printf( "<div class='it'><span class='test-name'>%s</span> ................... <span class='test-result %s'>%s</span> \n<br> %s </div>", $it, strtolower( $report->status ), $report->status, empty( $report->stack ) ? '' : $report->stack );
+		}
+
+		printf( '</div> </div>');
+	}
+}
+
+class Sugar_unit_test
 {
 	var $value;
 	var $mode = 'equal'; 	// equal, gt, gte, lt, lte
@@ -97,15 +171,17 @@ class Unit_test
 	var $equals;
 
 
-	function Unit_test( $value )
+	function Sugar_unit_test( $value )
 	{
 		$this->value = $value;
+		$this->start = microtime( TRUE );
 
 		$this->should =
 		$this->be =
 		$this->to =
 		$this->equal =
 		$this->equals =
+		$this->have =
 		$this;
 	}
 
@@ -145,7 +221,7 @@ class Unit_test
 	}
 
 
-	/* comparators */
+	/* Comparators */
 
 	function be( $value = NULL )
 	{
@@ -234,6 +310,8 @@ class Unit_test
 	}
 
 
+	/* Numbers */
+
 	function greater( $expected = NULL )
 	{
 		$this->mode = 'gt';
@@ -317,6 +395,149 @@ class Unit_test
 	}
 
 
+	function then( $expected )
+	{
+		return $this->_mode( $expected );
+	}
+
+
+	/* Bool */
+
+	function fail()
+	{
+		$this->thrown();
+	}
+
+
+	function true()
+	{
+		$this->exactly( TRUE );
+	}
+
+
+	function ok()
+	{
+		$this->equal( TRUE );
+	}
+
+
+	function false()
+	{
+		$this->exactly( FALSE );
+	}
+
+
+	/* String */
+
+	function contain( $search, $offset, $length )
+	{
+		if ( is_string( $this->value ) )
+		{
+			$result = ( substr_count( $this->value, $search, $offset, $limit ) > 0 );
+		}
+
+		else
+		{
+			$result = TRUE;
+
+			foreach( $search as $value )
+			{
+				if ( in_array( $value, $this->value ) == FALSE )
+				{
+					$result = FALSE;
+					break;
+				}
+			}
+		}
+
+		if ( $this->_can_continue( $result ))
+		{
+			return $this;
+		}
+
+		else
+		{
+			$this->_throw( $this->value, 'to contain', $search );
+		}
+		
+	}
+
+	function length( $expected )
+	{
+		if (  _can_continue( ( is_string( $this->value ) && strlen( $this->value ) == $expected ) || ( is_array( $this->value ) && count( $this->value ) == $expected ) ))
+		{
+			return $this;
+		}
+
+		else
+		{
+			$this->_throw( strlen( $this->value ), 'to be', $expected );
+		}
+	}
+
+
+	/* Array */
+
+	function keys( $expected )
+	{
+		$flag = TRUE;
+
+		foreach( $expected as $key )
+		{
+			if ( array_key_exists( $key, $this->value ) == FALSE )
+			{
+				$flag = FALSE;
+				break;
+			}
+		}
+
+		if ( _can_continue( $flag ) )
+		{
+			return $this;
+		}
+
+		else
+		{
+			$this->_throw( $this->value, 'to have keys', $expected );
+		}
+	}
+
+
+	function only( $expected )
+	{
+		$flag = TRUE;
+
+		if ( count( $this->value ) != count( $expected ) )
+		{
+			$flag = FALSE;
+		}
+
+		else
+		{
+			foreach( $expected as $key )
+			{
+				if ( array_key_exists( $key, $this->value ) == FALSE )
+				{
+					$flag = FALSE;
+					break;
+				}
+			}
+		}
+
+		if ( _can_continue( $flag ) )
+		{
+			return $this;
+		}
+
+		else
+		{
+			$this->_throw( $this->value, 'to have only keys', $expected );
+		}
+	}
+
+
+	/* Output */
+
 	function outputs( $expected = NULL )
 	{
 		## THIS MAY REWRITE RETURN VALUE
@@ -348,16 +569,18 @@ class Unit_test
 	}
 
 
+	/* Empty? */
+
 	function blank()
 	{
-		if ( ( ! $this->negation && empty( $this->value ) ) || ( $this->negation && ! empty( $this->value ) ) )
+		if ( $this->_can_continue( empty( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected empty, [%s] given.', $this->value ) );
+			$this->_throw( $this->value, 'to be', 'Empty' );
 		}
 	}
 
@@ -369,216 +592,174 @@ class Unit_test
 	}
 
 
-	function then( $expected )
-	{
-		return $this->_mode( $expected );
-	}
-
-
 	function thrown()
 	{
-		if ( ( ! $this->negation && $this->throw ) || ( $this->negation && ! $this->throw ))
+		if ( $this->_can_continue( $this->throw ))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Exception should be thrown, instead it has value [%s].', $this->value ) );
-		}
-	}
-
-
-	function fail()
-	{
-		$this->thrown();
-	}
-
-	function true()
-	{
-		$this->exactly( TRUE );
-	}
-
-	function ok()
-	{
-		$this->equal( TRUE );
-	}
-
-	function false()
-	{
-		$this->exactly( FALSE );
-	}
-
-	function exactly( $expected )
-	{
-		if ( $this->mode == 'equal' && ( ( ! $this->negation && $this->value === $expected ) || ( $this->negation && ! ( $this->value === $expected )) ) )
-		{
-			return $this;
-		}
-
-		else
-		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] not to be [%s].', $this->value, $expected ) );
+			$this->_throw( $this->value, 'to thrown', 'Exception' );
 		}
 	}
 
 
 	/* Data types */
 
-	function array( $expected )
+	function _array()
 	{
-		if ( ( ( ! $this->negation && is_array( $this->value )) || ( $this->negation && ! is_array( $this->value ) )))
+		if ( $this->_can_continue( is_array( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be array.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'array' );
 		}
 	}
 
 
-	function bool( $expected )
+	function _bool()
 	{
-		if ( ( ( ! $this->negation && is_bool( $this->value )) || ( $this->negation && ! is_bool( $this->value ) )))
+		if ( $this->_can_continue( is_bool( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be bool.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'bool' );
 		}
 	}
 
 
-	function callable( $expected )
+	function _callable()
 	{
-		if ( ( ( ! $this->negation && is_callable( $this->value )) || ( $this->negation && ! is_callable( $this->value ) )))
+		if ( $this->_can_continue( is_callable( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be callable.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'callable' );
 		}
 	}
 
 
-	function double( $expected )
+	function _double()
 	{
-		if ( ( ( ! $this->negation && is_double( $this->value )) || ( $this->negation && ! is_double( $this->value ) )))
+		if ( $this->_can_continue( is_double( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be double.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'double' );
 		}
 	}
 
 
-	function float( $expected )
+	function _float()
 	{
-		if ( ( ( ! $this->negation && is_float( $this->value )) || ( $this->negation && ! is_float( $this->value ) )))
+		if ( $this->_can_continue( is_float( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be float.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'float' );
 		}
 	}
 
 
-	function int( $expected )
+	function _int()
 	{
-		if ( ( ( ! $this->negation && is_int( $this->value )) || ( $this->negation && ! is_int( $this->value ) )))
+		if ( $this->_can_continue( is_int( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be int.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'int' );
 		}
 	}
 
 
-	function object( $expected )
+	function _object()
 	{
-		if ( ( ( ! $this->negation && is_object( $this->value )) || ( $this->negation && ! is_object( $this->value ) )))
+		if ( $this->_can_continue( is_object( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be object.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'object' );
 		}
 	}
 
 
-	function resource( $expected )
+	function _resource()
 	{
-		if ( ( ( ! $this->negation && is_resource( $this->value )) || ( $this->negation && ! is_resource( $this->value ) )))
+		if ( $this->_can_continue( is_resource( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be resource.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'resource' );
 		}
 	}
 
 
-	function scalar( $expected )
+	function _scalar()
 	{
-		if ( ( ( ! $this->negation && is_scalar( $this->value )) || ( $this->negation && ! is_scalar( $this->value ) )))
+		if ( $this->_can_continue( is_scalar( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] to be scalar.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'scalar' );
 		}
 	}
 
 
-	function number( $expected )
+	function _number()
 	{
-		if ( ( ( ! $this->negation && is_numeric( $this->value )) || ( $this->negation && ! is_numeric( $this->value ) )))
+		if ( $this->_can_continue( is_numeric( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] not to be number.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'numeric' );
 		}
 	}
 
 
-	function string( $expected )
+	function _string()
 	{
-		if ( ( ( ! $this->negation && is_string( $this->value )) || ( $this->negation && ! is_string( $this->value ) )))
+		if ( $this->_can_continue( is_string( $this->value )))
 		{
 			return $this;
 		}
 
 		else
 		{
-			throw new Exception( sprintf( 'Failed: Expected [%s] not to be string.', $this->value, $expected ) );
+			$this->_throw( gettype( $this->value), 'to be', 'string' );
 		}
 	}
-
-
 
 
 	/* Special */
@@ -589,6 +770,22 @@ class Unit_test
 		$this->negation = FALSE;
 
 		return $this;
+	}
+
+
+	/* Comparing */
+
+	function exactly( $expected )
+	{
+		if ( $this->mode == 'equal' && $this->_can_continue( $this->value === $expected ))
+		{
+			return $this;
+		}
+
+		else
+		{
+			$this->_throw( $this->value, 'to be exactly', $expected );
+		}
 	}
 
 
@@ -605,15 +802,22 @@ class Unit_test
 
 		else
 		{
-			if ( $this->negation )
-			{
-				throw new Exception( sprintf( 'Failed: Expected [%s] not to be [%s].', $this->value, $expected ) );
-			}
-
-			else
-			{
-				throw new Exception( sprintf( 'Failed: Expected [%s] to be [%s].', $this->value, $expected ) ); 
-			}
+			$this->_throw( $this->value, 'to be equal', $expected );
 		}
+	}
+
+	function _throw( $expected, $sentence, $given )
+	{
+		throw new Exception( sprintf(
+			'Failed: Excepted [%s] %s%s [%s].',
+			is_array( $expected ) ? ('[' . join( ', ', array_keys( $expected )) . ']') : $expected,
+			$this->negation ? 'not ' : '' ,
+			$sentence,
+			is_array( $given ) ? ('[' . join( ', ', array_keys( $given )) . ']') : $expected ));
+	}
+
+	function _can_continue( $value )
+	{
+		return ( ( ! $this->negation && $value ) || ( $this->negation && ! $value ) );
 	}
 }
